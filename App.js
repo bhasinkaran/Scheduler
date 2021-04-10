a435ad5c227bf0a5272d92ec33a4931078bf3fdb
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2021-04-10 16:48:57 -0500

    First commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>Welcome to my app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00f',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    color: '#fff',
+    fontSize: 32,
+  }
 });
